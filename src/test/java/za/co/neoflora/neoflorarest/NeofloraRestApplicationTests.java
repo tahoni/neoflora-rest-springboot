ce978c95fae4558468ae1e80ec052e81e7d82e79
Leoni Lubbinge <tahoni@gmail.com>
2023-04-09 13:36:03 +0200

    Initial Spring Boot 3.0.5 commit

---
+++ src/test/java/za/co/neoflora/neoflorarest/NeofloraRestApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.neoflora.neoflorarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeofloraRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
