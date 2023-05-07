c7f7d3a51deb477bc7eedff225accf81048b5e66
Leoni Lubbinge <tahoni@gmail.com>
2023-05-07 13:16:52 +0200

    Integrate swagger with the RESTful service

--- src/main/java/za/co/neoflora/NeofloraRestApplication.java
+++ src/main/java/za/co/neoflora/NeofloraRestApplication.java
@@ -1,11 +1,9 @@
-package za.co.neoflora.rest;
+package za.co.neoflora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"za.co.neoflora.dao"})
 public class NeofloraRestApplication {
 
 	public static void main(String[] args) {
